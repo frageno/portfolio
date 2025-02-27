498e8b21bb2715b27ed61659ece033455e1d47fc
frageno <skipperskladanka@gmail.com>
2025-02-27 18:35:37 +0100

    fix: some minor adjustment to the slider

--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -18,7 +18,7 @@ const RecentProject = () => {
             Sprawdz moje { '' }
             <span className="text-purple">ostatnie projekty!</span>
         </h2>
-        <div className="relative px-5">
+        <div className="relative px-8 lg:px-6">
         <Swiper
           modules={[Navigation]}
           navigation={{
@@ -31,14 +31,18 @@ const RecentProject = () => {
           className="w-full"
           breakpoints={{
             320: {
-              slidesPerView: 1.2,
+              slidesPerView: 1,
               spaceBetween: 16
             },
             768: {
-              slidesPerView: 1.75,
-              spaceBetween: 24
+              slidesPerView: 2,
+              spaceBetween: 16
             },
             1024: {
+              slidesPerView: 2,
+              spaceBetween: 24
+            },
+            1280: {
               slidesPerView: 3,
               spaceBetween: 32
             }
@@ -46,9 +50,9 @@ const RecentProject = () => {
         >
           {projects.map(({ id, title, des, img, link }) => (
             <SwiperSlide key={id}>
-              <div className="w-full flex items-center justify-center rounded-3xl p-8 border border-white/[0.1]">
+              <div className="w-full flex items-center justify-center rounded-3xl border border-white/[0.1]">
                 <div className="flex basis-full flex-col tracking-tight text-slate-100/50">
-                  <div className="relative w-full h-[300px] overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <div className="relative w-full h-[200px] lg:h-[300px] overflow-hidden rounded-t-3xl bg-[#13162d]">
                     <Image 
                       src={img} 
                       alt={title} 
@@ -60,7 +64,7 @@ const RecentProject = () => {
                     <div className="absolute inset-0 bg-gradient-to-br from-[#393bb2]/10 to-transparent"></div>
                   </div>
                   
-                  <div className="pt-6 space-y-2">
+                  <div className="p-6 lg:p-8 space-y-2">
                     <h3 className="font-bold text-base lg:text-xl text-slate-100">
                       {title}
                     </h3>
@@ -80,10 +84,10 @@ const RecentProject = () => {
           ))}
         </Swiper>
         {/* Custom arrows */}
-        <div className="swiper-button-prev !-left-8 !text-purple !w-8 !h-8">
+        <div className="swiper-button-prev !-left-3 md:!-left-6 lg:!-left-8 !text-purple !w-8 !h-8">
             <FiChevronsLeft className="w-full h-full" />
         </div>
-        <div className="swiper-button-next !-right-8 !text-purple !w-8 !h-8"> 
+        <div className="swiper-button-next !-right-3 md:!-right-6 lg:!-right-8 !text-purple !w-8 !h-8"> 
             <FiChevronsRight className="w-full h-full" />
         </div>
       </div>
